af91d70ceaba099bacaa2ebb6e672bef859d5f9e
Gabriele Consiglio <4226595+ilmeskio@users.noreply.github.com>
2025-11-07 10:32:03 +0100

    build: set vite base for pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base = repoName ? `/${repoName}/` : "/";
+
 export default defineConfig({
+  base,
   build: {
     rollupOptions: {
       input: {
